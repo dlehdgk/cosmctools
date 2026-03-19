221dd072bf9284728fd1ce6c6a9ac8e6719ad5e3
Dong Ha's MBP <2dongha.lee@gmail.com>
2026-03-19 04:00:30 +0000

    added chain blocks to get more reliable errors. Saving sampled parameters with the trained model

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ import equinox as eqx
 import sys
 from scipy.stats import genpareto
 from flax import serialization
-from .MCMC_Evidence.Cobaya_wrapper import *
+from MCMC_Evidence.Cobaya_wrapper import *
 
 
 class cosmo_model:
-    def __init__(self, root, burnin=0.3, nchains=4):
+    def __init__(self, root, burnin=0.3, nchains=4, block_num=32):
+        # base properties
         self.root = root
         self.burnin = burnin
-        self.nchains = nchains
-        self.display_params = None
-        self.sampled_params = None
+        self.nchains = nchains  # number of MCMC chains in root (change this later to automatically detect)
+        self.sampled_params = None  # list of params sampled by MCMC
 
         # properties for getdist
         self._getdist_samples = None
+        self.display_params = None  # list of params to display using getdist (implement later for plots too)
         self.chi2 = 0
 
         # properties for MCEvidence
@@ -28,12 +29,16 @@ class cosmo_model:
         self._mce_params = None
 
         # properties for harmonic
+        ## properties of the MCMC chains loaded into harmonic
+        self._hm_chains = None
+        self._block_num = block_num
         self.training = None
         self.inference = None
-        self.hm_evidence = None
-        self.hm_evidence_error = None
+
+        ## properties of the flow model and evidence
         self._hm_model = None
-        self._hm_chains = None
+        self._hm_evi = None
+        # self.hm_evidence_error = None
 
     # %% getdist
 
@@ -80,7 +85,7 @@ class cosmo_model:
 
     def add_derived(self):
         """
-        adding derived parameters not tracked by MCMC chains.
+        adding derived parameters not tracked by MCMC chains. Add your function here!
         """
         p = self.getdist_samples.getParams()
         # equation of the derived parameter here
@@ -120,7 +125,7 @@ class cosmo_model:
 
     def get_chi2(self):
         """
-        Get MAP ln likelihood = -1/2 chi2_eff for minimised MCMC chains. Requires a .minimum file from a minimiser sampler in Cobaya.
+        Get -1/2 MAP ln likelihood = chi2_eff for minimised MCMC chains. Requires a .minimum file from a minimiser sampler in Cobaya. Add names of observations to list as required.
         """
         bf = self.getdist_samples.getBestFit(max_posterior=True)
         dict = bf.getParamDict()
@@ -145,11 +150,11 @@ class cosmo_model:
 
     def Delta_chi2(self, alternative_model):
         """
-        returning the difference in chi2 between this model and an alternative model. > 0 implies alternative model is favoured, < 0 implies this model is favoured.
+        returning the difference in chi2 between this model and an alternative model. < 0 implies alternative model is favoured, > 0 implies this model is favoured.
         """
         chi2_1 = self.get_chi2()
         chi2_2 = alternative_model.get_chi2()
-        delta_chi2 = chi2_1 - chi2_2
+        delta_chi2 = chi2_2 - chi2_1
         # print(rf"$\Delta \chi^2$: {delta_chi2:.3f}")
         return delta_chi2
 
@@ -170,31 +175,36 @@ class cosmo_model:
 
     def get_MCE_bayes_factor(self, alternative_model):
         """
-        returning the Bayes factor between this model and an alternative model using MCEvidence. < 0 implies alternative model is favoured, > 0 implies this model is favoured.
+        returning the Bayes factor between this model and an alternative model using MCEvidence. > 0 implies alternative model is favoured, < 0 implies this model is favoured.
         """
         evidence1 = self.mcevidence
         evidence2 = alternative_model.mcevidence
-        bayes_factor = evidence1 - evidence2
+        bayes_factor = evidence2 - evidence1
         # print(rf"$\ln\mathcal{{Z}}_\mathrm{{MCE}}$: {bayes_factor:.3f}")
         return bayes_factor
 
     # %% Learned Harmonic Mean Estimator
+
     def _load_hm_chains(self, verbose=False):
         chains = hm.Chains(len(self.sampled_params))
         for i in range(self.nchains):
-            chain = pd.read_csv(self.root + f".{i + 1}.txt", sep=r"\s+")
+            with open(f"{self.root}.{i + 1}.txt", "r") as f:
+                header = f.readline().strip().lstrip("#").split()
             chain = pd.read_csv(
-                self.root + f".{i + 1}.txt",
+                f"{self.root}.{i + 1}.txt",
                 sep=r"\s+",
-                header=None,
-                skiprows=1,
-                names=chain.keys()[1 : len(chain.keys())],
+                comment="#",
+                names=header,
             )
             if verbose:
                 print(f"Chain {i + 1} loaded with shape: {chain.shape}")
                 print(f"Chain {i + 1} starts with:")
                 print(chain.head())
-            chain["logpost"] = -1 * chain["minuslogpost"]
+
+            # get log posterior
+            chain["logpost"] = -1.0 * chain["minuslogpost"]
+
+            # apply burnin based on cumulative weights
             cumulative_weights = chain["weight"].cumsum()
             total_steps = cumulative_weights.iloc[-1]
             if verbose:
@@ -202,19 +212,17 @@ class cosmo_model:
             burnin_steps = int(total_steps * self.burnin)
             start_index = (cumulative_weights >= burnin_steps).idxmax()
             post_burnin = chain.iloc[start_index:]
-            keep_params = ["weight", "logpost"] + self.sampled_params
-            if verbose:
-                print(f"Chain {i + 1} keeping only: {keep_params}")
-            filtred = post_burnin[keep_params]
-            weights = filtred["weight"].to_numpy().astype(int)
-            logpost_array = filtred["logpost"].to_numpy()
-            chain_array = filtred[self.sampled_params].to_numpy()
 
+            # expand by weight and add to harmonic chains
+            weights = post_burnin["weight"].to_numpy().astype(int)
+            logpost_array = post_burnin["logpost"].to_numpy()
+            chain_array = post_burnin[self.sampled_params].to_numpy()
             expanded_chain = np.repeat(chain_array, weights, axis=0)
             expanded_logpost = np.repeat(logpost_array, weights, axis=0)
             if verbose:
                 print(f"Chain {i + 1}: {len(expanded_chain)} samples after expansion")
             chains.add_chain(expanded_chain, expanded_logpost)
+        chains.split_into_blocks(self._block_num)
         return chains
 
     @property
@@ -231,13 +239,20 @@ class cosmo_model:
 
     def store_model(self):
         """
-        store trained model
+        store trained flow model.
         """
+
+        # store architecture
         np.savez(
             f"{self.root}.RQSpline.npz",
+            params=np.array(self._mce_params),
+            n_layers=self._hm_model.n_layers,
+            n_bins=self._hm_model.n_bins,
             offset=self._hm_model.pre_offset,
             amp=self._hm_model.pre_amp,
         )
+
+        # store learned states and weights
         with open(f"{self.root}.RQSpline_state.msgpack", "wb") as f:
             f.write(serialization.to_bytes(self._hm_model.state))
 
@@ -245,42 +260,91 @@ class cosmo_model:
             f.write(serialization.to_bytes(self._hm_model.variables))
         return self
 
+    def set_temp(self, temp, verbose=False):
+        """
+        set temperature of the learned distribution for harmonic mean estimation.
+        """
+        if temp > 1.0:
+            raise ValueError(
+                "Temperature should be less than or equal to 1.0 for harmonic mean estimation."
+            )
+        elif self._hm_model is None:
+            raise ValueError(
+                "Please load or train the model first before setting temperature."
+            )
+        else:
+            self._hm_model.temperature = temp
+        if verbose:
+            flow_samples = self._hm_model.sample(self.hm_chains.nsamples)
+            utils.plot_getdist_compare(self.hm_chains.samples, flow_samples)
+            plt.show()
+            print("ensure that the concentrated flow is contained within the posterior")
+        return self
+
     def load_model(self, temp=0.9, training_proportion=0.5):
         """
-        load trained model
+        load trained flow model.
         """
+        # load architecture
+        data = np.load(f"{self.root}.RQSpline.npz")
+
+        ## setting sampled params
+        self.set_sampled_params(data["params"].tolist())
+
+        # load hm_chains if needed
         self.training, self.inference = utils.split_data(
             self.hm_chains, training_proportion=training_proportion
         )
 
         skeleton = hm.model.RQSplineModel(
-            len(self.sampled_params), standardize=True, temperature=temp
+            len(self.sampled_params),
+            n_layers=int(data["n_layers"]),
+            n_bins=int(data["n_bins"]),
+            standardize=True,
+            temperature=1.0,
         )
 
         # dummy fit to initialise the model
-        skeleton.fit(self.training.samples[:10], epochs=1, verbose=False)
+        skeleton.fit(np.ones((1, len(self.sampled_params))), epochs=1, verbose=False)
 
-        data = np.load(f"{self.root}.RQSpline.npz")
+        ## setting stardardisation parameters
         skeleton.pre_offset = data["offset"]
         skeleton.pre_amp = data["amp"]
 
+        # load learned states and weights
         with open(f"{self.root}.RQSpline_state.msgpack", "rb") as f:
             skeleton.state = serialization.from_bytes(skeleton.state, f.read())
 
         with open(f"{self.root}.RQSpline_vars.msgpack", "rb") as f:
             skeleton.variables = serialization.from_bytes(skeleton.variables, f.read())
         self._hm_model = skeleton
+
+        # set temperature
+        self.set_temp(temp)
         return self
 
-    def train_model(self, training_proportion=0.5, epochs=15, temp=0.9, verbose=False):
+    def train_model(
+        self,
+        training_proportion=0.5,
+        epochs=15,
+        temp=0.9,
+        n_layers=3,
+        n_bins=128,
+        verbose=False,
+    ):
         """
         Training the RQSpline model for the Learned Harmonic Mean Estimator using the MCMC chains.
         """
+        # split into training and inference sets
         self.training, self.inference = utils.split_data(
             self.hm_chains, training_proportion=training_proportion
         )
         self._hm_model = hm.model.RQSplineModel(
-            len(self.sampled_params), standardize=True, temperature=temp
+            len(self.sampled_params),
+            n_layers=n_layers,
+            n_bins=n_bins,
+            standardize=True,
+            temperature=temp,
         )
         self._hm_model.fit(self.training.samples, epochs=epochs, verbose=verbose)
         if verbose:
@@ -291,37 +355,45 @@ class cosmo_model:
         self.store_model()
         return self
 
-    def get_hm_evidence(
-        self,
-        train_model=False,
-        **kwargs,
-    ):
+    def _hm_evi_obj(self):
+        evi = hm.Evidence(self.inference.nchains, self._hm_model)
+        evi.add_chains(self.inference)
+        self._hm_evi = evi
+        return self
+
+    def get_hm_evidence(self, train_model=False, **kwargs):
+        """
+        Get -lnZ and (lower, upper) errors on -lnZ
+        """
         if train_model:
             self.train_model(**kwargs)
         elif self._hm_model is None:
             self.load_model(**kwargs)
-        evi = hm.Evidence(self.inference.nchains, self._hm_model)
-        evi.add_chains(self.inference)
+        evi = self._hm_evi_obj()
         self.hm_evidence = evi.ln_evidence_inv
         self.hm_evidence_error = evi.compute_ln_inv_evidence_errors()
         return self.hm_evidence, self.hm_evidence_error
 
+    def check_evidence(self):
+        lnk = self._hm_evi.ln_kurtosis
+        print("=== Checking Reliability of Evidence ===")
+        print(f"ln kurtosis: {lnk:.4f} (>>1 implies evidence may be unreliable)")
+        return self
+
     def get_LMHE_bayes_factor(self, alternative_model, **kwargs):
         """
-        returning the Bayes factor between this model and an alternative using Harmonic. < 0 implies the alternative model is favoured, > 0 implies this model is favoured.
+        Returning the Bayes factor between this model and an alternative using Harmonic. > 0 implies the alternative model is favoured, < 0 implies this model is favoured.
         """
         evidence1, evidence1_err = self.get_hm_evidence(**kwargs)
         evidence2, evidence2_err = alternative_model.get_hm_evidence(**kwargs)
 
         # as harmonic gives -lnZ flip the order of subtraction
-        bayes_factor = evidence2 - evidence1
+        bayes_factor = evidence1 - evidence2
 
-        # Harmonic returns errors as [lower_err, upper_err].
-        # We take absolute values to ensure magnitudes are used in quadrature.
-        e1_err_lower, e1_err_upper = abs(evidence1_err[0]), abs(evidence1_err[1])
-        e2_err_lower, e2_err_upper = abs(evidence2_err[0]), abs(evidence2_err[1])
+        # flip upper and lower errors to get errors for lnZ
+        e1_err_lower, e1_err_upper = abs(evidence1_err[1]), abs(evidence1_err[0])
+        e2_err_lower, e2_err_upper = abs(evidence2_err[1]), abs(evidence2_err[0])
 
-        # Propagating asymmetric errors for subtraction (R = E2 - E1)
         # Upper error of result mixes E2's upper and E1's lower
         err_upper = np.sqrt(e2_err_upper**2 + e1_err_lower**2)
 
@@ -337,7 +409,7 @@ class cosmo_model:
 
     def check_harmonic_diagnostics(self):
         """
-        Objectively checks if the trained flow is strictly contained within the posterior
+        Checks if the trained flow is strictly contained within the posterior
         by analyzing the importance weights of the inference chains.
         """
         if self.inference is None or self._hm_model is None:
@@ -359,9 +431,6 @@ class cosmo_model:
         # Convert to linear weights safely
         weights = np.exp(log_weights - np.max(log_weights))
 
-        # Maximum Weight Fraction
-        max_weight_frac = np.max(weights) / np.sum(weights)
-
         # Kish's Effective Sample Size
         ess = np.sum(weights) ** 2 / np.sum(weights**2)
         fractional_ess = ess / len(weights)
@@ -374,7 +443,6 @@ class cosmo_model:
         k, *_ = genpareto.fit(shifted_tail, floc=0)
 
         print("=== Harmonic Estimator Diagnostics ===")
-        print(f"Max Weight Fraction: {max_weight_frac:.4f} (Should be < 0.01)")
         print(f"Fractional ESS:      {fractional_ess:.4f} (Higher is better)")
         print(f"Pareto-k Diagnostic: {k:.4f} (Less than 0.7 required)")
 
@@ -385,4 +453,4 @@ class cosmo_model:
         else:
             print("0.5 < Pareto-k < 0.7. May wish to adjust temperature and epochs.")
 
-        return k, fractional_ess
+        return self
